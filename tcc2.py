272fb513d07673f717073411c30da0b70804f9fa
Gabriel Mendanha <gabrielmendanha@hotmail.com>
2017-01-27 00:39:36 -0300

    Arquivo está sendo salvo no disco com sucesso.

--- tcc2.py
+++ tcc2.py
@@ -1,22 +1,30 @@
-from flask import Flask, render_template, jsonify, request, redirect
+
+
+from flask import Flask, render_template, jsonify, request
+from flask.ext.uploads import UploadSet, configure_uploads, AllExcept, SCRIPTS, EXECUTABLES
 from bigchaindb_driver.crypto import generate_keypair
-from werkzeug.utils import secure_filename
 from bigchaindb_driver import BigchainDB
+import os
 import ipfsapi
+from werkzeug.utils import secure_filename
+
 
 app = Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+UPLOAD_FOLDER = '/uploads/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+files = UploadSet('files', AllExcept(SCRIPTS + EXECUTABLES))
+
 api_endpoint = 'http://localhost:9984/api/v1'
+
+app.config['UPLOADS_DEFAULT_DEST'] = UPLOAD_FOLDER
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+app.config['DEBUG'] = True
 ipfs = ipfsapi.connect('127.0.0.1', 5001)
 bigchain = BigchainDB(api_endpoint)
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-app.debug = True
+configure_uploads(app, files)
 
 
 @app.route('/')
@@ -34,17 +42,28 @@ def generate_keys():
     return jsonify(key_pair)
 
 
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/upload', methods=['POST'])
 def upload():
     if request.method == 'POST':
-        print(request.form)
-        if 'file' not in request.files or request.files['file'].filename == '':
-            return redirect('/')
 
         file = request.files['file']
-        if allowed_file(file.filename):
-            print(ipfs.add(file))
-            return redirect('/generateKeys')
+        # Verifica se o arquivo veio
+        if 'file' not in request.files or file.filename == '':
+            return "deu pau"
+
+        if file and allowed_file(file.filename):
+
+            filename = secure_filename(file.filename)
+
+            destination = os.path.join(APP_ROOT+UPLOAD_FOLDER, filename)
+            file.save(destination)
+
+            return "ok"
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 if __name__ == '__main__':
     app.run()
