cc1f83ac29f85fa734c9e0ab399196de99acd4a8
Gabriel Mendanha <gabrielmendanha@hotmail.com>
2017-01-27 18:49:27 -0300

    Upload de arquivo para o IPFS OK, transacao no bigchainDB quase pronto. Falta utilizar as chaves informadas pelo usuario.

--- tcc2.py
+++ tcc2.py
@@ -1,33 +1,24 @@
-
-
-from flask import Flask, render_template, jsonify, request
-from flask.ext.uploads import UploadSet, configure_uploads, AllExcept, SCRIPTS, EXECUTABLES
+from flask import Flask, render_template, jsonify, request, redirect
 from bigchaindb_driver.crypto import generate_keypair
 from bigchaindb_driver import BigchainDB
-import os
-import ipfsapi
 from werkzeug.utils import secure_filename
-
+import ipfsapi
+import shutil
+import os
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = '/uploads/'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-files = UploadSet('files', AllExcept(SCRIPTS + EXECUTABLES))
-
-api_endpoint = 'http://localhost:9984/api/v1'
-
-app.config['UPLOADS_DEFAULT_DEST'] = UPLOAD_FOLDER
+API_ENDPOINT = 'http://localhost:9984/api/v1'
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-app.config['DEBUG'] = True
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app.config['DEBUG'] = True  # Comment this line before deploying
 ipfs = ipfsapi.connect('127.0.0.1', 5001)
-bigchain = BigchainDB(api_endpoint)
-configure_uploads(app, files)
+bigchain = BigchainDB(API_ENDPOINT)
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
     return render_template('index.html')
 
@@ -47,23 +38,57 @@ def upload():
     if request.method == 'POST':
 
         file = request.files['file']
-        # Verifica se o arquivo veio
+        public_key = request.form['pubKey']
+        private_key = request.form['privKey']
+
+        # Verify if file exists and is valid
         if 'file' not in request.files or file.filename == '':
-            return "deu pau"
+            return redirect('/')
 
         if file and allowed_file(file.filename):
 
             filename = secure_filename(file.filename)
+            base_destination = APP_ROOT + UPLOAD_FOLDER + public_key
+            destination = os.path.join(APP_ROOT + UPLOAD_FOLDER + public_key + '/', filename)
 
-            destination = os.path.join(APP_ROOT+UPLOAD_FOLDER, filename)
+            if not os.path.isdir(base_destination):
+                os.mkdir(base_destination)
+
             file.save(destination)
+            ipfs_file = ipfs.add(destination)
+            shutil.rmtree(base_destination)
 
-            return "ok"
+            payload = {
+                'data': {
+                    'Hash': ipfs_file['Hash'],
+                    'Name': ipfs_file['Name']
+                },
+            }
+            person = generate_keypair()
+            prepared_creation_tx = bigchain.transactions.prepare(
+                operation='CREATE',
+                owners_before=person.verifying_key,
+                asset=payload,
+            )
+
+            fulfilled_creation_tx = bigchain.transactions.fulfill(
+                prepared_creation_tx, private_keys=person.signing_key)
+
+            bigchain.transactions.send(fulfilled_creation_tx)
+
+            txid = fulfilled_creation_tx['id']
+
+            trials = 0
+            while bigchain.transactions.status(txid).get('status') != 'valid' and trials < 100:
+                trials += 1
+
+            return "ok " + txid + " " + str(bigchain.transactions.status(txid))
 
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 if __name__ == '__main__':
     app.run()
